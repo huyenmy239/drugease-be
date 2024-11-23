f9a9f3e28ef55af19d35f50767767abce23c6f71
mie <nguyenthihuyenmy239@gmail.com>
2024-11-24 05:07:49 +0700

    Add database setting and requirements file

--- drugease/drugease/settings.py
+++ drugease/drugease/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'drugease.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'drug',
+        'USER': 'root',
+        'PASSWORD': '239003',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
